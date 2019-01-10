b5f703cfc00b37514f604e4d9d6fc891180d8cba
kradme <coolang@yeah.net>
2019-01-10 12:30:15 +0800

    create

---
+++ src/main/java/cn/lang/randompwd/RandomPwdApplication.java
@@ -0,0 +1,14 @@
+package cn.lang.randompwd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomPwdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandomPwdApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/cn/lang/randompwd/RandomPwdApplicationTests.java
@@ -0,0 +1,17 @@
+package cn.lang.randompwd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RandomPwdApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
